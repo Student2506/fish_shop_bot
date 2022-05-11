854d474d18cbcef54237855c544419f989f29d82
Захаров Павел <pzakharov83@gmail.com>
2022-05-11 15:16:22 +0300

    remove test main from secondary module

--- api_elasticpath.py
+++ api_elasticpath.py
@@ -1,5 +1,4 @@
 import logging
-import os
 
 import requests
 
@@ -113,33 +112,3 @@ def create_customer_record(url, access_token, id, email):
     response = requests.post(url, headers=headers, json=json_data)
     response.raise_for_status()
     return response.json()
-
-
-def main():
-    logging.basicConfig(level=logging.DEBUG, format=FORMAT)
-    client_id = os.getenv('FISH_SHOP_CLIENT_ID')
-
-    access_token = get_token(client_id).get('access_token', None)
-    logger.debug(f'access_token: {access_token}')
-    goods = get_catalog('https://api.moltin.com/v2/products', access_token)
-    logger.debug(f'goods: {goods}')
-    cart = get_cart('https://api.moltin.com/v2/carts/', access_token)
-    logger.debug(f"it's cart: {cart.get('data', None).get('id', None)}")
-    cart = add_proudct_to_cart(
-        'https://api.moltin.com/v2/carts/',
-        'ed8f8b07-ceca-4983-bc0c-c6b5a36f1967',
-        4,
-        access_token
-    )
-    logger.debug(f'added products: {cart}')
-    cart = get_cart_products('https://api.moltin.com/v2/carts/', access_token)
-    logger.debug(f'cart_details: {cart}')
-    product_id = cart.get('data', None)[0].get('id', None)
-    cart = remove_products_from_cart(
-        'https://api.moltin.com/v2/carts/', product_id, access_token
-    )
-    logger.debug(f'removing {cart}')
-
-
-if __name__ == '__main__':
-    main()
